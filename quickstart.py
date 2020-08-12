96027ee4b4d7049be7126ffbf22c74392dbb8e92
Vijender <vijendernagra@Vijenders-MacBook-Pro.local>
2020-08-11 18:28:50 -0700

    updates to files

--- quickstart.py
+++ quickstart.py
@@ -1,20 +1,46 @@
-""" Quickstart script for InstaPy usage """
-
 # imports
 from instapy import InstaPy
 from instapy import smart_run
-from instapy import set_workspace
 
+# login credentials
+insta_username = 'ryd_den'
+insta_password = 'V220856s'
 
-# set workspace folder at desired location (default is at your home folder)
-set_workspace(path=None)
+comments = ['Nice shot! @{}',
+        'I love your profile! @{}',
+        'Your feed is an inspiration :thumbsup:',
+        'Just incredible :open_mouth:',
+        'What camera did you use @{}?',
+        'Love your posts @{}',
+        'Looks awesome @{}',
+        'Getting inspired by you @{}',
+        ':raised_hands: Yes!',
+        'I can feel your passion @{} :muscle:']
 
 # get an InstaPy session!
-session = InstaPy()
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=False,
+                  multi_logs=True)
 
 with smart_run(session):
-    # general settings
-    session.set_dont_include(["friend1", "friend2", "friend3"])
-
-    # activity
-    session.like_by_tags(["natgeo"], amount=10)
+  """ Activity flow """		
+  # general settings		
+  session.set_dont_include(["friend1", "friend2", "friend3"])		
+  
+  # activity
+  session.follow_by_tags(["motorcycle"], amount=2)
+  session.like_by_tags(["motorcycle"], amount=2)
+  session.follow_user_followers(["plexaudio"],amount=10, randomize=False)
+  session.set_do_like(enabled=True, percentage=100)
+  session.set_delimit_commenting(enabled=False, max=10, min=0)
+  session.set_do_follow(enabled=True, percentage=100, times=2)
+  session.set_do_comment(False, percentage=20)
+  session.set_relationship_bounds(enabled=True,
+  delimit_by_numbers=True,
+  max_followers=1000,
+  max_following=900,
+  min_followers=20,
+  min_following=10)
+ 
